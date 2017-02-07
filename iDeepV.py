cbeda9dddec3037dbaf09730f80cb5f96201532a
panxy <xypan172436@gmail.com>
2017-02-07 10:45:49 +0100

    update

--- iDeepV.py
+++ iDeepV.py
@@ -698,65 +698,6 @@ def get_rna_seqs_rec(rnas, rna_seq_dict, rna_nax_len, trids, nn_dict):
             rna_array.append(tri_feature_shu)
     
     return np.array(rna_array), np.array(label)
-  
-def run_rnacomend(datadir = 'data/', ushuffle = True):
-    fw = open('result_file_rbp67', 'w')
-    pair_file = datadir + 'test_part2'
-    #rbp_seq_file = datadir + 'rbps_HT.fa'
-    rna_seq_file = datadir + 'utrs.fa'
-     
-    rna_seq_dict = read_fasta_file(rna_seq_file)
-    rna_len = [] # 2695
-    for val in rna_seq_dict.values():
-        rna_len.append(len(val))
-    
-    rna_len.sort()
-    rna_nax_len = rna_len[int(len(rna_len)*0.8)] 
-    seq_hid = 16
-    label = []
-    rna_array = []
-    protein_pair = {}
-    trids = get_6_trids()
-    nn_dict = read_rna_dict()
-    with open(pair_file, 'r') as fp:
-        for line in fp:
-            values = line.rstrip().split()
-            protein = values[0]
-            rna = values[1]
-            protein_pair.setdefault(protein, []).append(rna)
-    
-    for protein, rnas in protein_pair.iteritems():
-        print protein
-        fw.write(protein + '\t')
-        data, label = get_rna_seqs_rec(rnas, rna_seq_dict, rna_nax_len, trids, nn_dict)
-        seq_net = get_cnn_network_graphprot(rna_len = rna_nax_len - 5, nb_filter = seq_hid)
-
-        training_val_indice, train_val_label, test_indice, test_label = split_training_validation(label)
-        train_val = data[training_val_indice]
-        train_val_label = label[training_val_indice]  
-        test_data = data[test_indice]
-        test_label = label[test_indice]
-        
-        training_indice, training_label, test_indice, test_label = split_training_validation(train_val_label)
-        cnn_train = train_val[training_indice]
-        training_label = train_val_label[training_indice]  
-        cnn_validation = train_val[test_indice]
-        validation_label = train_val_label[test_indice]
-                 
-        y, encoder = preprocess_labels(training_label)
-        val_y, encoder = preprocess_labels(validation_label, encoder = encoder) 
-        print 'predicting'    
-        model_name = 'model/' + protein +'.pickle'
-        seq_auc, seq_predict = calculate_auc(seq_net, seq_hid, cnn_train, test_data, test_label, y, validation = cnn_validation,
-                                              val_y = val_y, model_name = model_name)
-        print str(seq_auc)
-        fw.write( str(seq_auc) +'\n')
-        mylabel = "\t".join(map(str, test_label))
-        myprob = "\t".join(map(str, seq_predict))  
-        fw.write(mylabel + '\n')
-        fw.write(myprob + '\n')
-        
-    fw.close()
     
 def read_seq_graphprot(seq_file, label = 1):
     seq_list = []
